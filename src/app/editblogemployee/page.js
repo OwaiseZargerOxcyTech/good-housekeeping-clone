730b0afc495ffc216d4628a88f4968d774ea7743
ZahidKhanOxytech <zahid.khan@oxytech.com>
2024-04-24 18:04:10 +0530

    added vercel blob and vercel configuration

--- src/app/editblogemployee/page.js
+++ src/app/editblogemployee/page.js
@@ -8,6 +8,7 @@ import Footer from "@/components/footer/Footer";
 import { useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import CryptoJS from "crypto-js";
+import { Suspense } from "react";
 
 const DynamicSunEditor = dynamic(() => import("suneditor-react"), {
   ssr: false,
@@ -175,149 +176,151 @@ const EditBlog = () => {
 
   return (
     <>
-      <div className=" px-6 py-10 sm:px-8 sm:py-16 ">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-10 sm:gap-x-10">
-          <div className=" col-span-3 space-y-10">
-            <SideNav />
-          </div>
+      <Suspense>
+        <div className=" px-6 py-10 sm:px-8 sm:py-16 ">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-10 sm:gap-x-10">
+            <div className=" col-span-3 space-y-10">
+              <SideNav />
+            </div>
 
-          <div className="col-span-9">
-            <div className="card w-full bg-base-100 rounded-md">
-              <form className="card-body">
-                <h1 className="pt-4 text-center text-3xl font-semibold">
-                  Edit Blog Details
-                </h1>
-                <label className="form-control w-full">
-                  <div className="label">
-                    <span className="label-text">Title</span>
-                  </div>
-                  <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    className="input input-bordered w-full placeholder-gray-500"
-                  />
-                </label>
-
-                <label className="form-control w-full">
-                  <div className="label">
-                    <span className="label-text">Meta Description</span>
-                  </div>
-                  <textarea
-                    type="text"
-                    id="desc"
-                    name="desc"
-                    value={desc}
-                    onChange={(e) => setDesc(e.target.value)}
-                    className="textarea textarea-bordered placeholder-gray-500"
-                    placeholder="Meta Description"
-                  ></textarea>
-                </label>
-
-                <label className="form-control w-full">
-                  <div className="label">
-                    <span className="label-text">Category</span>
-                  </div>
-                </label>
-
-                <select
-                  onChange={handleCategoryChange}
-                  value={selectedCategoryName || ""}
-                  className="select select-bordered w-full"
-                >
-                  <option disabled value="">
-                    Select Category?
-                  </option>
-                  {categories.map((category) => (
-                    <option key={category.name}>{category.name}</option>
-                  ))}
-                  {selectedCategoryName === "" && (
-                    <option disabled style={{ display: "none" }}>
-                      Select Category?
-                    </option>
-                  )}
-                </select>
-
-                <label className="form-control w-full">
-                  <div className="label">
-                    <span className="label-text">Blog Content</span>
-                  </div>
-                </label>
-
-                <DynamicSunEditor
-                  onChange={setContent}
-                  setContents={content}
-                  placeholder="Blog Content"
-                  className="text-black"
-                  height="300px"
-                  setOptions={{
-                    height: "100%", // Use px unit for height
-                    buttonList: [
-                      ["undo", "redo"],
-                      [
-                        "bold",
-                        "underline",
-                        "italic",
-                        "strike",
-                        "subscript",
-                        "superscript",
-                      ],
-                      ["removeFormat"],
-                      ["outdent", "indent"],
-                      ["fullScreen", "showBlocks", "codeView"],
-                      ["preview", "print"],
-                      ["link", "image", "video"],
-                      [
-                        "font",
-                        "fontSize",
-                        "formatBlock",
-                        "align",
-                        "list",
-                        "table",
-                      ],
-                      ["fontColor", "hiliteColor", "horizontalRule"],
-                    ],
-                    font: ["Arial", "Courier New"], // Example: specify fonts
-                    fontColor: "red", // Set font color
-                    backgroundColor: "red", // Set background color
-                  }}
-                />
-                <div className="mt-6">
-                  <label
-                    htmlFor="image"
-                    className="p-2 border border-gray-300 cursor-pointer text-gray-500 hover:text-blue-700"
-                  >
-                    <span>
-                      {imageName ? imageName : "Upload New Blog Image"}
-                    </span>
+            <div className="col-span-9">
+              <div className="card w-full bg-base-100 rounded-md">
+                <form className="card-body">
+                  <h1 className="pt-4 text-center text-3xl font-semibold">
+                    Edit Blog Details
+                  </h1>
+                  <label className="form-control w-full">
+                    <div className="label">
+                      <span className="label-text">Title</span>
+                    </div>
                     <input
-                      type="file"
-                      id="image"
-                      name="image"
-                      onChange={handleImageChange}
-                      className="hidden"
+                      type="text"
+                      id="title"
+                      name="title"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                      className="input input-bordered w-full placeholder-gray-500"
                     />
                   </label>
-                </div>
 
-                <div className="flex justify-end">
-                  <button
-                    onClick={handleBlogUpdate}
-                    className="btn bg-[#dc2626] w-20 text-white"
+                  <label className="form-control w-full">
+                    <div className="label">
+                      <span className="label-text">Meta Description</span>
+                    </div>
+                    <textarea
+                      type="text"
+                      id="desc"
+                      name="desc"
+                      value={desc}
+                      onChange={(e) => setDesc(e.target.value)}
+                      className="textarea textarea-bordered placeholder-gray-500"
+                      placeholder="Meta Description"
+                    ></textarea>
+                  </label>
+
+                  <label className="form-control w-full">
+                    <div className="label">
+                      <span className="label-text">Category</span>
+                    </div>
+                  </label>
+
+                  <select
+                    onChange={handleCategoryChange}
+                    value={selectedCategoryName || ""}
+                    className="select select-bordered w-full"
                   >
-                    Save
-                  </button>
-                </div>
-              </form>
+                    <option disabled value="">
+                      Select Category?
+                    </option>
+                    {categories.map((category) => (
+                      <option key={category.name}>{category.name}</option>
+                    ))}
+                    {selectedCategoryName === "" && (
+                      <option disabled style={{ display: "none" }}>
+                        Select Category?
+                      </option>
+                    )}
+                  </select>
+
+                  <label className="form-control w-full">
+                    <div className="label">
+                      <span className="label-text">Blog Content</span>
+                    </div>
+                  </label>
+
+                  <DynamicSunEditor
+                    onChange={setContent}
+                    setContents={content}
+                    placeholder="Blog Content"
+                    className="text-black"
+                    height="300px"
+                    setOptions={{
+                      height: "100%", // Use px unit for height
+                      buttonList: [
+                        ["undo", "redo"],
+                        [
+                          "bold",
+                          "underline",
+                          "italic",
+                          "strike",
+                          "subscript",
+                          "superscript",
+                        ],
+                        ["removeFormat"],
+                        ["outdent", "indent"],
+                        ["fullScreen", "showBlocks", "codeView"],
+                        ["preview", "print"],
+                        ["link", "image", "video"],
+                        [
+                          "font",
+                          "fontSize",
+                          "formatBlock",
+                          "align",
+                          "list",
+                          "table",
+                        ],
+                        ["fontColor", "hiliteColor", "horizontalRule"],
+                      ],
+                      font: ["Arial", "Courier New"], // Example: specify fonts
+                      fontColor: "red", // Set font color
+                      backgroundColor: "red", // Set background color
+                    }}
+                  />
+                  <div className="mt-6">
+                    <label
+                      htmlFor="image"
+                      className="p-2 border border-gray-300 cursor-pointer text-gray-500 hover:text-blue-700"
+                    >
+                      <span>
+                        {imageName ? imageName : "Upload New Blog Image"}
+                      </span>
+                      <input
+                        type="file"
+                        id="image"
+                        name="image"
+                        onChange={handleImageChange}
+                        className="hidden"
+                      />
+                    </label>
+                  </div>
+
+                  <div className="flex justify-end">
+                    <button
+                      onClick={handleBlogUpdate}
+                      className="btn bg-[#dc2626] w-20 text-white"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <footer>
-        <Footer />
-      </footer>
+        <footer>
+          <Footer />
+        </footer>
+      </Suspense>
     </>
   );
 };
